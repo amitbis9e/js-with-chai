3c25ae0804f505e932c31ff9c3f8a6a6a705e247
amitbis9e <148409640+amitbis9e@users.noreply.github.com>
2026-05-29 12:48:28 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // using JS code newer vesion
+//alert(3+2)  // using nodejs , not the broswer
+console.log("Premative dataTypes")
+// string
+// boolen
+// null
+// undefined
+// symbol --> used for uniqueness
+
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
+console.log(typeof Symbol)
+console.log(typeof Boolean)
