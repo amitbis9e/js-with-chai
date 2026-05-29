b8e6ae224595341ea8a0dc442512e8822d7057c1
amitbis9e <148409640+amitbis9e@users.noreply.github.com>
2026-05-29 05:12:55 +0000

    continer config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountMail = "ncert@gmail.com"
+var accountPassword = "12345"
+accountCity = "bombay"
+
+console.log(accountId)
+console.table([accountId, accountMail, accountPassword, accountCity])
